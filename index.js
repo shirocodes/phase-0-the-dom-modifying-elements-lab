6c67a2b70b273cd47d6efd1ecde3d94da1a9e5e4
shirocodes <ishirapt@gmail.com>
2025-03-01 15:51:29 +0300

    Removed and added an element with an id and text

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Write your code here!
+
+const main = document.querySelector("main#main");
+main.remove();
+
+const newHeader = document.createElement("h1");
+const nodeName = document.createTextNode("H1");
+newHeader.appendChild(nodeName);
+
+newHeader.id = "victory";
+
+newHeader.innerHTML = "Shiro is the champion"
